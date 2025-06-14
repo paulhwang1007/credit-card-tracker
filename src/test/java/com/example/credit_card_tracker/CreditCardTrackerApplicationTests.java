68caca54b037a9c14d797bc7c12b959a0972d16f
Paul Hwang <hwpaul1007@gmail.com>
2025-06-13 19:10:19 -0700

    Create Springboot File + Git Setup

---
+++ src/test/java/com/example/credit_card_tracker/CreditCardTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.credit_card_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
