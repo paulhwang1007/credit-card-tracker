d9f125028d9e78e4186e8e017443c124934d5bf9
Paul Hwang <hwpaul1007@gmail.com>
2025-06-14 02:27:40 -0700

    Create Repository Interface for CRUD Operations

---
+++ src/main/java/com/example/credit_card_tracker/CreditCardRepository.java
@@ -0,0 +1,7 @@
+package com.example.credit_card_tracker;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CreditCardRepository extends JpaRepository<CreditCard, Integer> {
+
+}
