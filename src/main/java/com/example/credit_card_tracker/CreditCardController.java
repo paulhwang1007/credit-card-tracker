e43042d6f60e9b80147e167a8fceb7c6f32d9d3e
Paul Hwang <hwpaul1007@gmail.com>
2025-06-29 21:51:58 -0700

    Add @CrossOrigin to enable CORS: Allow Frontend to request API calls to Backend

--- src/main/java/com/example/credit_card_tracker/CreditCardController.java
+++ src/main/java/com/example/credit_card_tracker/CreditCardController.java
@@ -2,6 +2,7 @@ package com.example.credit_card_tracker;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping(path = "api/v1/credit_card")
 public class CreditCardController {
