31d9a5da646eddc782835faff66b05ed7e6b7c0c
Paul Hwang <hwpaul1007@gmail.com>
2025-07-14 22:03:00 -0700

    Change to Spring JPA Hibernate to Update and Comment out Dummy Data

--- src/main/java/com/example/credit_card_tracker/CreditCardConfig.java
+++ src/main/java/com/example/credit_card_tracker/CreditCardConfig.java
@@ -14,31 +14,31 @@ public class CreditCardConfig {
     CommandLineRunner commandLineRunner(CreditCardRepository repository) {
         return args -> {
 
-            CreditCard discover_it = new CreditCard(
-                    "Discover iT",
-                    "Capital One",
-                    0,
-                    LocalDate.of(2023, Month.NOVEMBER, 4),
-                    List.of("5% Cashback on quarterly select shops",
-                            "1% Cashback on all other purchase"),
-                    "Unlimited Cashback on all purchases for the 1st Year"
-            );
-
-            CreditCard chase_ful = new CreditCard(
-                    "Freedom Unlimited",
-                    "Chase",
-                    0,
-                    LocalDate.of(2025, Month.MARCH, 23),
-                    List.of("3% Cashback on Dining, including takeout and delivery",
-                            "3% Cashback on Drugstore purchases",
-                            "5% Cashback on travel booked through Chase Travel",
-                            "1.5% Cashback on all other purchases"),
-                    "$200 bonus in points after spending $500 in the first 3 months"
-            );
-
-            repository.saveAll(
-                    List.of(discover_it, chase_ful)
-            );
+//            CreditCard discover_it = new CreditCard(
+//                    "Discover iT",
+//                    "Capital One",
+//                    0,
+//                    LocalDate.of(2023, Month.OCTOBER, 20),
+//                    List.of("5% Cashback on quarterly select shops",
+//                            "1% Cashback on all other purchase"),
+//                    "Unlimited Cashback on all purchases for the 1st Year"
+//            );
+//
+//            CreditCard chase_ful = new CreditCard(
+//                    "Freedom Unlimited",
+//                    "Chase",
+//                    0,
+//                    LocalDate.of(2025, Month.MARCH, 12),
+//                    List.of("3% Cashback on Dining, including takeout and delivery",
+//                            "3% Cashback on Drugstore purchases",
+//                            "5% Cashback on travel booked through Chase Travel",
+//                            "1.5% Cashback on all other purchases"),
+//                    "$200 bonus in points after spending $500 in the first 3 months"
+//            );
+//
+//            repository.saveAll(
+//                    List.of(discover_it, chase_ful)
+//            );
 
         };
     }
